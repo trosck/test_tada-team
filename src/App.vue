b1fd29b3cbffeea6d0cd7ab25243052746bcdb57
Sergey Belov <kort.dll@gmail.com>
2022-06-14 12:51:20 +0300

    ref: app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,22 @@
 <template>
-  <div class="root" ref="root">
-    <div class="content">
-      <VueHeader />
-      <router-view class="content__main" />
-    </div>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <router-view/>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import VueHeader from "@/components/Base/VueHeader"
-
 export default {
-  components: {
-    VueHeader,
-  },
-}
+  name: 'App',
+};
 </script>
-
-<style lang="scss" scoped>
-.root {
-  min-width: 100vw;
-  min-height: 100vh;
-
-  max-width: 100vw;
-  max-height: 100vh;
-
-  background-color: #021827;
-}
-
-.content {
-  max-width: 500px;
-  min-height: 100vh;
-  max-height: 100vh;
-  overflow: auto;
-  margin: auto;
-  background-color: #02193B;
-
-  display: flex;
-  flex-direction: column;
-}
-
-.content__main {
-  flex-grow: 1;
-  overflow: auto;
-}
-</style>

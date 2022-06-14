a2eeaecb6bc8affb66eec48da7013afa372368b1
Sergey Belov <kort.dll@gmail.com>
2022-06-14 06:37:38 +0300

    ref: store refactoring

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,7 @@ import Vuex from "vuex"
 
 Vue.use(Vuex)
 
-import actions from "./actions"
-import getters from "./getters"
-import mutations from "./mutations"
+import { byDateAsc } from "@/plugins/sort"
 
 export default new Vuex.Store({
   state: {
@@ -19,7 +17,84 @@ export default new Vuex.Store({
       name: "",
     }
   },
-  actions,
-  getters,
-  mutations,
+
+  mutations: {
+    setUsername(state, username) {
+      state.username = username
+    },
+    setLogin(state, value) {
+      state.isLogin = value
+    },
+    addMessage(state, message) {
+      state.chatList.push(message)
+    },
+    setSocket(state, socket) {
+      state.socket = socket
+    },
+    setLoading(state, value) {
+      state.loading = value
+    },
+  },
+
+  actions: {
+    loginUser({ commit }, username) {
+      commit("setUsername", username)
+      commit("setLogin", true)
+    },
+
+    resolveMessageData({ commit, getters }, event) {
+      const username = getters.username
+      try {
+        const data = JSON.parse(event.data)
+  
+        if (data.typing) return
+  
+        data._type = data.name ? "message" : "invited"
+        data._id = Date.now()
+        data._self = data.name === username
+  
+        commit("addMessage", data)
+      } catch(error) {
+        console.error(error)
+      }
+    },
+
+    initWebSocket({ commit, dispatch, getters }) {
+  
+      commit("setLoading", true)
+  
+      const username = getters.username
+      const badData = JSON.stringify({
+        data: { text: "Connection is closed" } 
+      })
+  
+      try {
+        const ws = new WebSocket(`${process.env.VUE_APP_CHAT_WS_ADRESS}/ws?name=${username}`)
+        ws.onmessage = event => dispatch("resolveMessageData", event)
+        ws.onopen = () => commit("setLoading", false)
+        ws.onclose = () => dispatch("resolveMessageData", badData)
+        ws.onerror = () => dispatch("resolveMessageData", badData)
+  
+        commit("setSocket", ws)
+      } catch(error) {
+        console.error(error)
+        commit("setLoading", false)
+      }
+    },
+
+    sendMessage({ state }, text) {
+      state.socket.send(
+        JSON.stringify({ text }) 
+      )
+    },
+  },
+
+  getters: {
+    isLogin: state => state.isLogin,
+    username: state => state.username,
+    loading: state => state.loading,
+    chatListAscSorted: state => state.chatList.sort(
+      (a, b) => byDateAsc(a.created, b.created)
+    ),
+  },
 })

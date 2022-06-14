597fa72d15ab21358ab53e50d3e389803cfedd67
Sergey Belov <kort.dll@gmail.com>
2022-06-14 19:24:25 +0300

    ref: update header, set max width

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,16 @@
       app
       color="primary"
       dark
+      class="header"
     >
+      <a href="/" class="text-h5 title">
+        vue-ws-chat <v-icon>mdi-github</v-icon>
+      </a>
     </v-app-bar>
 
     <v-main>
       <v-container>
-        <router-view/>
+        <router-view class="content"/>
       </v-container>
     </v-main>
   </v-app>
@@ -20,3 +24,14 @@ export default {
   name: 'App',
 };
 </script>
+
+<style lang="scss" scoped>
+.title {
+  color: white;
+  text-decoration: none;
+}
+.header, .content {
+  max-width: 500px;
+  margin: auto;
+}
+</style>
